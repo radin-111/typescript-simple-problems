f4618f883c02235775e3c3257d8562b6105d5a30
radin-111 <radinsheikh87@gmail.com>
2025-11-17 19:48:07 +0600

    adding 1st problem"

--- solution.ts
+++ solution.ts
@@ -1,1 +1,14 @@
-console.log('Hello')
+// Problem 1
+
+function formatValue(value: number | string | boolean) {
+  if (typeof value === "number") {
+    value = value * 10;
+  } else if (typeof value === "string") {
+    value = value.toUpperCase();
+  } else {
+    return !value;
+  }
+  return value;
+}
+
+// Problem 2

15dd457f86d49575c149e8aed4bf53d387874b31
radin-111 <radinsheikh87@gmail.com>
2025-11-17 21:15:39 +0600

    adding 4th problem and updating functions

--- solution.ts
+++ solution.ts
@@ -1,6 +1,6 @@
 // Problem 1
 
-function formatValue(value: number | string | boolean) {
+const formatValue = (value: number | string | boolean):number | string | boolean => {
   if (typeof value === "number") {
     value = value * 10;
   } else if (typeof value === "string") {
@@ -9,17 +9,31 @@ function formatValue(value: number | string | boolean) {
     return !value;
   }
   return value;
-}
+};
 
 // Problem 2
 
-function getLength(value: string | any[]) {
+const getLength = (value: string | any[]):number => {
+  let size: number = 0;
   if (typeof value === "string") {
-    return value.length;
+    size = value.length;
   }
 
   if (Array.isArray(value)) {
-    return value.length;
+    size = value.length;
   }
-}
+
+  return size;
+};
+
+// Problem 4
+ 
+const filterByRating = (
+  arr: Array<{ title: string; rating: number }>
+): Array<object> => {
+  const filteredArray = arr.filter(
+    (item: { title: string; rating: number }) => item.rating >= 4
+  );
+  return filteredArray;
+};
 
